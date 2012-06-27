4705892a73398801519b34a3c31aedc73a78ad66
Stew Hildebrand <stewart@stew.dk>
2012-06-27 15:20:03 -0400

    Wheelchair - functional LCD driver written

--- wheelchair/src/lcd_driver.c
+++ wheelchair/src/lcd_driver.c
@@ -5,17 +5,101 @@
  *  Author: Stew
  */ 
 
+#include "../atmel/avr_compiler.h"
 #include <avr/io.h>
+#include <string.h>
+#include "lcd_driver.h"
 
-/* LCD DB0:7 = PC0:7
- * LCD E = PD1
- * LCD RW = PD4
- * LCD RS = PD5
- */
+static inline void lcdESet(void)
+{
+	LCD_E_PORT.OUTSET = LCD_E_PIN_bm;
+}
 
-void initLCDDriver(void) {
-	PORTC.DIRSET = 0xFF;
-	PORTC.OUTCLR = 0xFF;
-	PORTD.DIRSET = PIN1_bm | PIN4_bm | PIN5_bm;
-	PORTD.OUTCLR = PIN1_bm | PIN4_bm | PIN5_bm;
+static inline void lcdEClr(void)
+{
+	LCD_E_PORT.OUTCLR = LCD_E_PIN_bm;
+}
+
+static inline void lcdRSSet(void)
+{
+	LCD_RS_PORT.OUTSET = LCD_RS_PIN_bm;
+}
+
+static inline void lcdRSClr(void)
+{
+	LCD_RS_PORT.OUTCLR = LCD_RS_PIN_bm;
+}
+
+static void lcdCommand(uint8_t command)
+{
+	LCD_DATA_PORT.OUT = command;
+	lcdRSClr();
+	lcdESet();
+	_delay_us(1);
+	lcdEClr();
+	if ((command == LCD_CMD_CLEAR_DISPLAY) || ((command & 0xFE) == LCD_CMD_RETURN_HOME)) {
+		_delay_ms(1.52);
+	} else {
+		_delay_us(38);
+	}
+}
+
+static void lcdWrite(uint8_t data)
+{
+	LCD_DATA_PORT.OUT = data;
+	lcdRSSet();
+	lcdESet();
+	_delay_us(1);
+	lcdEClr();
+	_delay_us(38);
+}
+
+static inline void lcdSetAddr(uint8_t addr)
+{
+	lcdCommand(LCD_CMD_SET_DDRAM_ADDR | (LCD_DDRAM_ADDR_bm & addr));
+}
+
+static void lcdLine(const char *text, uint8_t startAddr) {
+	size_t i;
+	size_t textlen = strlen(text);
+	if (textlen > 16) {
+		textlen = 16;
+	}
+	lcdSetAddr(startAddr);
+	for (i = 0; i < textlen; i++) {
+		lcdWrite(text[i]);
+	}
+}
+
+void lcdText(const char *line1, const char *line2)
+{
+	lcdLine(line1, LCD_LINE_1_START_ADDR);
+	lcdLine(line2, LCD_LINE_2_START_ADDR);
+}
+
+void initLCDDriver(void)
+{
+	LCD_DATA_PORT.DIRSET = 0xFF;
+	LCD_DATA_PORT.OUTCLR = 0xFF;
+	LCD_RS_PORT.DIRSET = LCD_RS_PIN_bm;
+	LCD_RS_PORT.OUTCLR = LCD_RS_PIN_bm;
+	LCD_RW_PORT.DIRSET = LCD_RW_PIN_bm;
+	LCD_RW_PORT.OUTCLR = LCD_RW_PIN_bm;
+	LCD_E_PORT.DIRSET = LCD_E_PIN_bm;
+	LCD_E_PORT.OUTCLR = LCD_E_PIN_bm;
+
+	lcdEClr();
+	_delay_ms(40); //Wait >40 msec after power is applied
+	lcdCommand(LCD_CMD_FUNCTION_SET | LCD_CMD_FUNCTION_DL_bm | LCD_CMD_FUNCTION_N_bm); //Wake up #1
+	_delay_ms(5);
+	lcdCommand(LCD_CMD_FUNCTION_SET | LCD_CMD_FUNCTION_DL_bm | LCD_CMD_FUNCTION_N_bm); //Wake up #2
+	_delay_us(160);
+	lcdCommand(LCD_CMD_FUNCTION_SET | LCD_CMD_FUNCTION_DL_bm | LCD_CMD_FUNCTION_N_bm); //Wake up #3
+	_delay_us(160);
+	lcdCommand(LCD_CMD_FUNCTION_SET | LCD_CMD_FUNCTION_DL_bm | LCD_CMD_FUNCTION_N_bm); // 8-bit bus, 2-line LCD
+	lcdCommand(LCD_CMD_DISPLAY_ON_OFF | LCD_CMD_DISPLAY_ON_OFF_D_bm); // | LCD_CMD_DISPLAY_ON_OFF_C_bm); //Display ON
+	lcdCommand(LCD_CMD_ENTRY_MODE_SET | LCD_CMD_ENTRY_MODE_I_D_bm); //Cursor moves right
+	lcdCommand(LCD_CMD_CLEAR_DISPLAY); // Last thing to do before writing text
+
+	lcdText("PWCT", "Ver. 2012-06-27");
 }
--- wheelchair/src/lcd_driver.h
+++ wheelchair/src/lcd_driver.h
@@ -5,9 +5,94 @@
  *  Author: Stew
  */ 
 
-
 #ifndef LCD_DRIVER_H_
 #define LCD_DRIVER_H_
+
+// BEGIN HARDWARE SPECIFIC
+
+// DB0:7 = PC0:7
+#define LCD_DATA_PORT PORTC
+
+// Operation Enable, E = PD1
+#define LCD_E_PORT PORTD
+#define LCD_E_PIN_bm PIN1_bm
+
+// Read/Write, RW = PD4
+#define LCD_RW_PORT PORTD
+#define LCD_RW_PIN_bm PIN4_bm
+//RW=0: write
+//RW=1: read WARNING READ NOT SUPPORTED BY HARDWARE
+
+// Register Select, RS = PD5
+#define LCD_RS_PORT PORTD
+#define LCD_RS_PIN_bm PIN5_bm
+//RS=0: instruction
+//RS=1: data
+
+// END HARDWARE SPECIFIC
+
+
+// Instructions/Commands
+
+#define LCD_CMD_CLEAR_DISPLAY 0x01
+	// Execution time = 1.52msec
+	// Also returns home
+
+#define LCD_CMD_RETURN_HOME 0x02
+	// Execution time = 1.52msec
+
+#define LCD_CMD_ENTRY_MODE_SET 0x04
+	// Execution time = 38usec
+#define LCD_CMD_ENTRY_MODE_I_D_bm 0x02
+	// I/D=1: cursor moves right / DDRAM address increments / display shift left
+	// I/D=0: cursor moves left / DDRAM address decrements / display shift right
+#define LCD_CMD_ENTRY_MODE_S_bm 0x01
+	// S=0: don't shift display
+	// S=1: during DDRAM write operation, shift display
+
+#define LCD_CMD_DISPLAY_ON_OFF 0x08
+	// Execution time = 38usec
+#define LCD_CMD_DISPLAY_ON_OFF_D_bm 0x04
+	// D=1: display on
+	// D=0: display off
+#define LCD_CMD_DISPLAY_ON_OFF_C_bm 0x02
+	// C=1: cursor on
+	// C=0: cursor off
+#define LCD_CMD_DISPLAY_ON_OFF_B_bm 0x01
+	// B=1: cursor blink on
+	// B=0: cursor blink off
+
+#define LCD_CMD_CURSOR_OR_DISPLAY_SHIFT 0x10
+	// Execution time = 38usec
+#define LCD_CMD_CURSOR_OR_DISPLAY_SHIFT_S_C_bm 0x08
+	// S/C=0: shift cursor
+	// S/C=1: shift display
+#define LCD_CMD_CURSOR_OR_DISPLAY_SHIFT_R_L_bm 0x04
+	// R/L=0: shift left
+	// R/L=1: shift right
+
+#define LCD_CMD_FUNCTION_SET 0x20
+	// Execution time = 38usec
+#define LCD_CMD_FUNCTION_DL_bm 0x10
+	// DL=1: 8 bit bus
+	// DL=0: 4 bit bus
+#define LCD_CMD_FUNCTION_N_bm 0x08
+	// N=0: 1-line display
+	// N=1: 2-line display
+#define LCD_CMD_FUNCTION_F_bm 0x04
+	// F=0: 5x8
+	// F=1: 5x11
+
+#define LCD_CMD_SET_CGRAM_ADDR 0x40
+	// Execution time = 38usec
+#define LCD_CGRAM_ADDR_bm 0x3F
+
+#define LCD_CMD_SET_DDRAM_ADDR 0x80
+	// Execution time = 38usec
+#define LCD_DDRAM_ADDR_bm 0x7F
+
+#define LCD_LINE_1_START_ADDR 0x00
+#define LCD_LINE_2_START_ADDR 0x40
 
 void initLCDDriver(void);
 
